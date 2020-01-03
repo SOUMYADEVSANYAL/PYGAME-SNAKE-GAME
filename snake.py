b4c2669bba41cee460f194b758264bd8983739eb
Soumyadev Sanyal <soumya.sp35@gmail.com>
2020-01-04 00:35:19 +0530

    Static food made for snake

--- snake.py
+++ snake.py
@@ -1,6 +1,8 @@
 
 
 import pygame
+import random
+
 pygame.init()
 
 # Colors
@@ -24,10 +26,15 @@ snake_x = 45
 snake_y = 55
 velocity_x = 0
 velocity_y = 0
+
+food_x = random.randint(0, screen_width)
+food_y = random.randint(0, screen_height)
+
 snake_size = 10
 fps = 30
 
 clock = pygame.time.Clock()
+
 # Game Loop
 while not exit_game:
     for event in pygame.event.get():
@@ -36,29 +43,30 @@ while not exit_game:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                velocity_x = 5
+                velocity_x = 10
                 velocity_y = 0
 
             if event.key == pygame.K_LEFT:
-                velocity_x = - 5
+                velocity_x = - 10
                 velocity_y = 0
 
             if event.key == pygame.K_UP:
-                velocity_y = - 5
+                velocity_y = - 10
                 velocity_x = 0
 
             if event.key == pygame.K_DOWN:
-                velocity_y = 5
+                velocity_y = 10
                 velocity_x = 0
 
     snake_x = snake_x + velocity_x
     snake_y = snake_y + velocity_y
 
     gameWindow.fill(white)
+    pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
     pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
     pygame.display.update()
     clock.tick(fps)
 
-
 pygame.quit()
 quit()
+
