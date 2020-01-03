5fec749868426ef4ccce8b3136e57812dfd50bbe
Soumyadev Sanyal <soumya.sp35@gmail.com>
2020-01-03 23:55:39 +0530

    Snake head created

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 
+
 import pygame
 pygame.init()
 
@@ -19,20 +20,23 @@ pygame.display.update()
 # Game specific variables
 exit_game = False
 game_over = False
+snake_x = 45
+snake_y = 55
+snake_size = 10
 
+clock = pygame.time.Clock()
 # Game Loop
 while not exit_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_game = True
 
-
-
     gameWindow.fill(white)
+    pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
     pygame.display.update()
-
 
 
 pygame.quit()
 quit()
+
 
