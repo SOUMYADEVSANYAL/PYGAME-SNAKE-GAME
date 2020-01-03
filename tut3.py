e94557444c4339a9deb3b409cde2019539e49d18
Soumyadev Sanyal <soumya.sp35@gmail.com>
2020-01-03 23:29:16 +0530

    Event Types and Key Press Handling In Pygame

--- tut3.py
+++ tut3.py
@@ -12,7 +12,12 @@ game_over = False
 # Creating a game loop
 while not exit_game:
     for event in pygame.event.get():
-        print(event)
+        if event.type == pygame.QUIT:
+            exit_game = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                print("You have pressed right arrow key")
 
 pygame.quit()
 quit()
