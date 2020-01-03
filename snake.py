895efd8852b379a2338161a01c740e5e24b83d03
Soumyadev Sanyal <soumya.sp35@gmail.com>
2020-01-04 01:33:45 +0530

    Snakes Game: Length Increment Logic

--- snake.py
+++ snake.py
@@ -27,46 +27,82 @@ snake_y = 55
 velocity_x = 0
 velocity_y = 0
 
-food_x = random.randint(0, screen_width)
-food_y = random.randint(0, screen_height)
-
+food_x = random.randint(20, screen_width/2)
+food_y = random.randint(20, screen_height/2)
+score = 0
+init_velocity = 1
 snake_size = 10
-fps = 30
+food_size = 5
+fps = 60
 
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 55)
+
+def text_screen(text, color, x, y):
+    screen_text = font.render(text, True, color)
+    gameWindow.blit(screen_text, [x,y])
+
+
+def plot_snake(gameWindow, color, snk_list, snake_size):
+    for x,y in snk_list:
+        pygame.draw.rect(gameWindow, color, [x, y, snake_size, snake_size])
+
+
+snk_list = []
+snk_length = 1
 
 # Game Loop
 while not exit_game:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_game = True
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                velocity_x = 10
+                velocity_x = init_velocity
                 velocity_y = 0
 
             if event.key == pygame.K_LEFT:
-                velocity_x = - 10
+                velocity_x = - init_velocity
                 velocity_y = 0
 
             if event.key == pygame.K_UP:
-                velocity_y = - 10
+                velocity_y = - init_velocity
                 velocity_x = 0
 
             if event.key == pygame.K_DOWN:
-                velocity_y = 10
+                velocity_y = init_velocity
                 velocity_x = 0
 
     snake_x = snake_x + velocity_x
     snake_y = snake_y + velocity_y
 
+    if abs(snake_x - food_x)<8 and abs(snake_y - food_y)<8:
+        score +=1
+        food_x = random.randint(20, screen_width / 2)
+        food_y = random.randint(20, screen_height / 2)
+        snk_length +=5
+
     gameWindow.fill(white)
-    pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
-    pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
+    text_screen("Score: " + str(score * 10), red, 5, 5)
+    pygame.draw.rect(gameWindow, red, [food_x, food_y, food_size, food_size])
+
+
+    head = []
+    head.append(snake_x)
+    head.append(snake_y)
+    snk_list.append(head)
+
+    if len(snk_list)>snk_length:
+        del snk_list[0]
+
+    # pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
+    plot_snake(gameWindow, black, snk_list, snake_size)
     pygame.display.update()
     clock.tick(fps)
 
 pygame.quit()
 quit()
+
 
