e53136fbe663239b17a0e52ce328d4456c62a3c3
Soumyadev Sanyal <soumya.sp35@gmail.com>
2020-01-03 22:49:43 +0530

    Game specific variable created

--- tut3.py
+++ tut3.py
@@ -1,4 +1,12 @@
 import pygame
 x=pygame.init()
 print(x)
+
+#Creating window
 gameWindow=pygame.display.set_mode((1200,500))
+pygame.display.set_caption("My First Game")
+
+# Game specific variables
+exit_game = False
+game_over = False
+
